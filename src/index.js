25a27c557eaa4705085a1922e63e99bf4e4ffd41
Satya Prakash Dwivedi <dwivediprakashsatya36@gmail.com>
2024-05-18 12:10:21 +0530

    utils code

--- src/index.js
+++ src/index.js
@@ -9,7 +9,19 @@ dotenv.config({
 
 
 connectDB()
+.then( () => {
+  app.on( (error) => {
+    console.log("error at app.on", error);
+    throw error;
+  })
 
+  app.listen(process.env.PORT || 8000 , () => {
+    console.log(`Server is running at port ${process.env.PORT}`);
+  })
+})
+.catch((err) => {
+  console.log(`MonogDB connection failed !!! `, err);
+})
 
 
 
